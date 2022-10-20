ba1345172f4acb08be26d8b345615fbea8b383de
prowebdev110 <tieosong119@gmail.com>
2022-10-21 00:37:18 +0900

    Update countdown timer.js

--- checkout-new version/assets/js/countdown timer.js
+++ checkout-new version/assets/js/countdown timer.js
@@ -21,9 +21,6 @@ let minutesSpan = document.createElement('span'),
     s = parseInt(dataSeconds) > max ? max : parseInt(dataSeconds),
     m = parseInt(dataMinutes) > max ? max : parseInt(dataMinutes);
 
-secondsSpan.classList.add('time');
-minutesSpan.classList.add('time');
-separator2.classList.add('separator');
 separator2.textContent = separatorValue;
 
 checkValue = (value)=>{
